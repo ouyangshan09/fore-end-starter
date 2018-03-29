1eaa7d81726a37a3678965a023624922fbca65e0
Ouyangshan <ouyangshan09@gmail.com>
2018-03-29 20:57:34 +0800

    improve:
    添加默认配置
    
    by Ouyang

---
+++ config/webpack.default.ts
@@ -0,0 +1,69 @@
+/**
+ * Created by Ouyang on 2018/3/29.
+ * webpack基础配置
+ * @author Ouyang
+ */
+import Webpack, { Configuration } from 'webpack';
+import Path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import Config from './config';
+
+const defaultConfig: Configuration = {
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            test: /\.(ts|tsx)$/,
+            exclude: /(node_modules|lib)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    'presets': ['env', { 'modules': false }],
+                    'env': {
+                        'test': {
+                            'presets': ['env']
+                        }
+                    }
+                    // plugins: []
+                }
+            },{
+                loader: 'awesome-typescript-loader'
+            }]
+        }, {
+            test: /\.s?([ca])ss$/,
+            exclude: [
+                Path.join(Config.root, 'public'),
+                Path.join(Config.root, 'node_modules'),
+                Path.join(Config.root, 'lib')
+            ],
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'postcss-loader', 'sass-loader'],
+                fallback: 'style-loader'
+            })
+        }, {
+            test: /\.(png|jpg|gif)$/,
+            loader: 'url-loader',
+            options: { limit: 8192 }
+        }]
+    },
+    plugins: [
+        new Webpack.DllReferencePlugin({
+            context: Config.root,
+            manifest: require(Path.join(Config.root, 'lib/vendor-manifest.jso'))
+        }),
+        new HtmlWebpackPlugin({
+            template: Path.join(Config.src, 'index.html'),
+            title: 'ts-starter'
+        }),
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            disable: false,
+            allChunks: true
+        })
+    ]
+};
+
+export default defaultConfig;
