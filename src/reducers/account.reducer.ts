9ed3739616f740a3a585be402f14883d9ccdb077
Ouyang09 <510475607@qq.com>
2018-06-20 18:01:56 +0800

    移除无用代码

--- src/reducers/account.reducer.ts
+++ src/reducers/account.reducer.ts
@@ -13,32 +13,6 @@ import {
     ReducerMap
 } from 'redux-actions';
 
-// const handles: ReducersMapObject<any, Action> = {
-//     'TEST_ACCOUNT': (state: any, action) => {
-//         console.log('test reducer:', state);
-//         return state
-//     }
-// };
-
-// export default function (state: any = {test: 'ouyang'}, action: Action): Reducer {
-//     console.log('state1:', state);
-//     console.log('state2:', action);
-//     if (handles[action.type]) {
-//         return handles[action.type](state, action);
-//     }
-//     return state;
-// };
-
-// function createReducer (initialState: any, handles: any): any {
-//     return function reducer(state = initialState, action: any) {
-//         if (handles.hasOwnProperty(action.type)) {
-//             return handles[action.type](state, action);
-//         }
-//         console.log('state:', state);
-//         return state;
-//     }
-// }
-
 const handles: ReducerMap<any, any> = {
     ['TEST_ACCOUNT']: (state, action) => {
         console.log('test_account:', state)
