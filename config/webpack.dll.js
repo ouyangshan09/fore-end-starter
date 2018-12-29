5048e48c4e88211fa8c6c9b280611299732b5e07
ouyangshan09 <ouyangshan09@gmail.com>
2018-12-29 10:25:43 +0800

    feat: 配置打包dll

---
+++ config/webpack.dll.js
@@ -0,0 +1,38 @@
+/**
+ * 常用依赖打包，加快编译速度
+ *
+ * @author Ouyang
+*/
+
+const config = require('./config');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        vendor: [
+            'react',
+            'react-dom',
+            'react-css-modules',
+            'react-dom',
+            'react-redux',
+            'react-router',
+            'react-router-dom',
+            'redux',
+            'redux-actions',
+        ]
+    },
+    output: {
+        path: config.lib,
+        filename: '[name].dll.js',
+        library: '[name]_library'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            context: config.root,
+            path: path.join(config.lib, '[name]-manifest.json'),
+            name: '[name]_library'
+        })
+    ],
+    mode: 'development'
+};
