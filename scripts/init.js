a407ad074a5a2b22dbfda391d799e784f3335a24
ouyangshan09 <ouyangshan09@gmail.com>
2018-12-29 18:52:50 +0800

    完善

--- scripts/init.js
+++ scripts/init.js
@@ -3,6 +3,10 @@
  * 
  * @author Ouyang
 */
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const os = require('os');
 
 process.on('unhandledRejection', err => {
     throw err;
@@ -12,9 +16,33 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
+function checkAppName (name) {
+    //
+}
+
 module.exports = function (
-    appPath,
-    appName
+    name,
+    verbose,
+    template
 ) {
-    
+    const root = path.resolve(name);
+    const appName = path.basename(root);
+
+    checkAppName(appName);
+    fs.ensureDirSync(name);
+
+    console.log(`Creating a new React Project in ${chalk.green(root)}.`);
+    console.log();
+
+    const packageJson = {
+        name: appName,
+        version: '0.1.0',
+        private: true
+    };
+    fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson, null, 2) + os.EOL);
+
+    // 命令行目录
+    const originalDirectory = process.cwd();
+    // 改变工作目录
+    process.chdir(root);
 }
